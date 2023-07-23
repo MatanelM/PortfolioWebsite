e49f8d33a5f6894f117d29d529af6e6d30d6d9b6
Matanel Mastorov <MatanelM12321@gmail.com>
2023-07-23 21:49:07 +0300

    first commit

---
+++ items.js
@@ -0,0 +1,164 @@
+const items = [
+    {
+        "date": "2023",
+        "name": "Android Sound Manager",
+        "description": "Library for android written in Java that can be helpful for implementing sounds in the application.",
+        "link": "https://github.com/MatanelM/AndroidSoundManager",
+        "embeddedLink": null,
+        "logo": "asm.svg",
+        "linkText": "Link to repo"
+    },
+    {
+        "date": "2023",
+        "name": "Glove for Accessibility",
+        "description": "A vibrating glove that translates music to vibrations and LED lights. It was made in a techno music hackathon and won 1st place.\n",
+        "link": "https://github.com/YourUsername/GloveForAccessibility",
+        "embeddedLink": null,
+        "logo": "glove.png",
+        "linkText": "Link to article"
+    },
+    {
+        "date": "2023",
+        "name": "Android HW2",
+        "description": "A vibrating glove that translates music to vibrations and LED lights. It was made in a techno music hackathon and won 1st place.\n",
+        "link": "https://github.com/MatanelM/23A10357-HW1",
+        "embeddedLink": "https://www.youtube.com/embed/WF3uICleATY",
+        "logo": "spaceship.svg",
+        "linkText": "Link to repo"
+    },
+    {
+        "date": "2023",
+        "name": "Version Control System for Linux",
+        "description": "A software for archiving text-based files. It is written in C programming language and C++ and have graphical UI with python. The project also works for windows with Docker installed.",
+        "link": "https://github.com/YourUsername/VersionControlSystemForLinux",
+        "embeddedLink": null,
+        "logo": "archive.png",
+        "linkText": "Link to repo"
+    },
+    {
+        "date": "2023",
+        "name": "Application Moderation Module",
+        "description": "Android java module for messages moderation using openai moderation api.",
+        "link": "https://github.com/MatanelM/ApplicationModerationModule",
+        "embeddedLink": null,
+        "logo": "eye.png",
+        "linkText": "Link to repo"
+    },
+    {
+        "date": "2022",
+        "name": "Hama-mami",
+        "description": "A miniature model for card accessed physical storage. The project had mechanical, electrical and software features, and was developed in Smart-Up program.\n",
+        "link": "",
+        "embeddedLink": "https://dms-exp2.licdn.com/playlist/vid/C4E05AQFAhx1ck9bk6g/mp4-720p-30fp-crf28/0/1654070036752?e=1690477200&v=beta&t=JeEH1jh6Z-N6SGNQtO2bwEK-BuwHJb6-nTiRWkSFArQ",
+        "logo": "3d-leaf.png",
+        "linkText": ""
+    },
+    {
+        "date": "2022",
+        "name": "Airport Manager",
+        "description": "A software to schedule flights. Made with C programming language. The data of the program saved to binary files or compressed text files.\n",
+        "link": "https://github.com/YourUsername/AirportManager",
+        "embeddedLink": null,
+        "logo": "management.png",
+        "linkText": "Link to repo"
+    },
+    {
+        "date": "2023",
+        "name": "Android Accessability Module",
+        "description": "Android library that offers accessible UI components with adjustable text sizes for enhanced readability in Android applications.",
+        "link": "https://github.com/MatanelM/AndroidAccessabilityModule",
+        "embeddedLink": "https://user-images.githubusercontent.com/33670032/254681552-72039d42-c1e9-4b10-aed0-bf21ac7465b1.mp4",
+        "logo": "text-analytics.png",
+        "linkText": "Link to repo"
+    },
+    {
+        "date": "2023",
+        "name": "IOS War game",
+        "description": null,
+        "link": "https://github.com/MatanelM/IOSHomeWork",
+        "embeddedLink": "https://github.com/MatanelM/IOSHomeWork/assets/33670032/8543a930-713c-4dee-8942-d44d225eb66a            ",
+        "logo": "king-of-hearts.png",
+        "linkText": "Link to repo"
+    },
+    {
+        "date": "2022",
+        "name": "ChessNotations",
+        "description": "The program gets .pgn files, reads the chess games and then saves the data in the db. The user then can see the data in GUI.",
+        "link": "https://github.com/MatanelM/ChessNotations",
+        "embeddedLink": null,
+        "logo": "chess.png",
+        "linkText": "Link to repo"
+    },
+    {
+        "date": "2022",
+        "name": "WhatsGame",
+        "description": "A chat platform to help you find your game team mate",
+        "link": "https://github.com/MatanelM/WhatsGame",
+        "embeddedLink": null,
+        "logo": "chat.png",
+        "linkText": "Link to repo"
+    },
+    {
+        "date": "2023",
+        "name": "Family Business",
+        "description": "Completion project of the Unity game development course of Afeka College of Engineering in Tel Aviv. The project features multiplayer game with various of interactions, weapon switching and looting.",
+        "link": "https://youtu.be/ZO2xTs1vLQY",
+        "embeddedLink": "https://www.youtube.com/embed/ZO2xTs1vLQY",
+        "logo": "familybusiness.png",
+        "linkText": "Link to youtube"
+    },
+    {
+        "date": "2020-present",
+        "name": "CSS Battle",
+        "description": "CSS Battle helps front end developers learn css in a creative way. The battle is about making a figure that is not so genuine only with css.",
+        "link": "https://cssbattle.dev/player/matanelmast",
+        "embeddedLink": null,
+        "logo": "logo-square.png",
+        "linkText": "Link to profile"
+    },
+    {
+        "date": "2020",
+        "name": "Ask me",
+        "description": "I developed a social system for sharing summaries and videos between students of Afeka, in which I was responsible for server and the client side. The project won the second place in Afeka's Hackathon as I was in my first year to college.",
+        "link": "https://drive.google.com/file/d/1pVpl2PKA0EaB8DszWdPtQ1yNfhNbDMVg/view",
+        "embeddedLink": null,
+        "logo": "askme.png",
+        "linkText": "Link to certificate"
+    },
+    {
+        "date": "2018",
+        "name": "Mine Sweeper",
+        "description": "The famous game - Mine Sweeper. Enjoy!",
+        "link": "https://matanelm.github.io/MineSweeper/",
+        "embeddedLink": null,
+        "logo": "mineSweeper.png",
+        "linkText": "Link to game"
+    },
+    {
+        "date": "present",
+        "name": "Leetcode",
+        "description": "Leetcode is a website with many code challenges, and helping job candidates to perform better in job interviews.",
+        "link": "https://leetcode.com/Matanelm/",
+        "embeddedLink": null,
+        "logo": "leetcode.png",
+        "linkText": "Link to profile"
+    },
+    {
+        "date": "2020",
+        "name": "Physics calculator",
+        "description": "Two projects - The first calculates a vector resultant and shows the result on a graph, and the second is a kinematics calculator resolver. ",
+        "link": "https://physics-app-3c4c3.web.app/vectors",
+        "embeddedLink": null,
+        "logo": "accounting.png",
+        "linkText": "Link to website"
+    },
+    {
+        "date": "",
+        "name": "Contact",
+        "description": "Send an email in the following link",
+        "link": "mailto:Matanelm12321@gmail.com?subject=Hello&body=This%20is%20the%20body%20of%20the%20email.",
+        "embeddedLink": null,
+        "logo": "gmail.png",
+        "linkText": "Send a message"
+    }
+]
